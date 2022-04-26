0109b090715db7d1e5f02731e23d26b5290118fb
Petr Nikolaev <npa040493@gmail.com>
2022-04-26 18:59:28 +0300

    Add two variants of auction, nedd to add tests and check all cases.

---
+++ contracts/BritishAuctionPlatform.sol
@@ -0,0 +1,106 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+contract BritishAuctionPlatform {
+    struct Rate {
+        address buyer;
+        uint256 rate;
+    }
+    struct Auction {
+        string item;
+        uint256 startPrice;
+        Rate maxRate;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 duration;
+        address seller;
+        bool finished;
+    }
+
+    address public owner;
+    uint8 private constant FEE = 5;
+    uint32 private constant DURATION = 5 days;
+    Auction[] public auctions;
+
+    event AuctionCreated(
+        uint256 index,
+        address seller,
+        uint256 startPrice,
+        string item,
+        uint256 duration
+    );
+    event AuctionFinished(
+        uint256 index,
+        address buyer,
+        uint256 endPrice,
+        string item
+    );
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function rewardComission() external payable onlyOwner {
+        require(address(this).balance > 0, "No money, no honey)");
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function createAuction(
+        string memory _item,
+        uint256 _startPrice,
+        uint256 _duration,
+        uint256 _sale
+    ) public {
+        require(msg.sender != owner, "Owner can't create auction");
+        uint256 duration = _duration == 0 ? DURATION : _duration;
+        uint256 endTime = block.timestamp + duration;
+
+        Rate memory maxRate = Rate({buyer: address(0), rate: _startPrice});
+
+        Auction memory auction = Auction({
+            item: _item,
+            startPrice: _startPrice,
+            startTime: block.timestamp,
+            maxRate: maxRate,
+            endTime: endTime,
+            duration: duration,
+            finished: false,
+            seller: msg.sender
+        });
+
+        auctions.push(auction);
+
+        emit AuctionCreated(
+            auctions.length - 1,
+            msg.sender,
+            _startPrice,
+            _item,
+            duration
+        );
+    }
+
+    function setYouRate(uint256 index, uint256 rate) public payable {
+        Auction storage auction = auctions[index];
+        require(auction.startPrice < rate, "You rate less than start price");
+        require(auction.maxRate.rate < rate, "You rate don't maximum");
+        Rate memory maxRate = Rate({buyer: msg.sender, rate: rate});
+        if (auction.maxRate.buyer != address(0) && auction.maxRate.rate > 0) {
+            payable(auction.maxRate.buyer).transfer(auction.maxRate.rate);
+        }
+        auction.maxRate = maxRate;
+    }
+
+    function auctionFinish(uint256 index) public payable onlyOwner {
+        Auction storage auction = auctions[index];
+        require(auction.maxRate.buyer != address(0), "No buyer, no money");
+        auction.finished = true;
+        payable(auction.seller).transfer(
+            auction.maxRate.rate - (auction.maxRate.rate * FEE) / 100
+        );
+    }
+}
---
+++ contracts/NetherlandsAuctionPlatform.sol
@@ -0,0 +1,107 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+contract NetherLandsAuctionPlatform {
+    struct Auction {
+        string item;
+        uint256 startPrice;
+        uint256 endPrice;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 duration;
+        uint256 sale;
+        address seller;
+        bool finished;
+    }
+
+    address public owner;
+    uint8 private constant FEE = 5;
+    uint32 private constant DURATION = 5 days;
+
+    event AuctionCreated(
+        uint256 index,
+        address seller,
+        uint256 startPrice,
+        string item,
+        uint256 duration
+    );
+    event AuctionFinished(
+        uint256 index,
+        address buyer,
+        uint256 endPrice,
+        string item
+    );
+
+    Auction[] public auctions;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function rewardComission() external payable onlyOwner {
+        require(address(this).balance > 0, "No money, no honey)");
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function createAuction(
+        string memory _item,
+        uint256 _startPrice,
+        uint256 _duration,
+        uint256 _sale
+    ) public {
+        require(msg.sender != owner, "Owner can't create auction");
+        uint256 duration = _duration == 0 ? DURATION : _duration;
+        require(_startPrice > duration * _sale, "Too cheap item");
+        uint256 endTime = block.timestamp + duration;
+
+        Auction memory auction = Auction({
+            item: _item,
+            startPrice: _startPrice,
+            endPrice: _startPrice - _sale * duration,
+            startTime: block.timestamp,
+            endTime: endTime,
+            duration: duration,
+            finished: false,
+            sale: _sale,
+            seller: msg.sender
+        });
+
+        auctions.push(auction);
+
+        emit AuctionCreated(
+            auctions.length - 1,
+            msg.sender,
+            _startPrice,
+            _item,
+            duration
+        );
+    }
+
+    function getCurrentPrice(uint256 index) public view returns (uint256) {
+        Auction memory auction = auctions[index];
+        return
+            auction.startPrice -
+            (block.timestamp - auction.startTime) *
+            auction.sale;
+    }
+
+    function buyItem(uint256 index) public payable {
+        uint256 currentPrice = getCurrentPrice(index);
+        require(msg.value >= currentPrice, "You don't have enough money");
+        Auction storage auction = auctions[index];
+        require(msg.sender != auction.seller, "Seller can't buy item");
+        auction.endPrice = currentPrice;
+        auction.finished = true;
+        auction.endTime = block.timestamp;
+        payable(auction.seller).transfer(
+            currentPrice - (currentPrice * FEE) / 100
+        );
+        payable(msg.sender).transfer(msg.value - currentPrice);
+        emit AuctionFinished(index, msg.sender, currentPrice, auction.item);
+    }
+}
